4ae2d6bb21f2743bbb8d424d67b497265e54ceea
Cappy <caapppaa@gmail.com>
2023-09-21 20:21:52 +1000

    Added a whole bunch of things, styling, extra js functionality, an about page, etc

--- main.js
+++ main.js
@@ -5,9 +5,26 @@ let songPart = ["chorus", "intro", "solo", "verse"];
 
 
 function Generate() {
-    let i = Math.floor(Math.random() * 10);;
-    let j = Math.floor(Math.random() * 3);
-    let data = (`Today you will be learning the ${songPart[j]} of the song ${songs[i]} by the ${artists[i]}`);
+    let i = Math.floor(Math.random() * 11);;
+    let j = Math.floor(Math.random() * 4);
+    let phrase = Math.floor(Math.random() * 4);
+    let data = "initialisation Error";
+
+    if (phrase === 1) {
+        data = (`Today you will be learning the ${songPart[j]} of the song ${songs[i]} by ${artists[i]}`);
+    }
+    else if (phrase === 2) {
+        data = (`Today you play the ${songPart[j]} of ${songs[i]} by the legendary ${artists[i]}`);
+    }
+    else if (phrase === 3) {
+        data = (`${songs[i]} is the song that will the learn the ${songPart[j]} from, if you don't know, it's by ${artists[i]}`);
+    }
+    else if (phrase === 4) {
+        data = (`Please learn the ${songPart[j]} in ${songs[i]} by the ${artists[i]}`);
+    }
+    else {
+        data = (`Today you commit to learning the ENTIRE ${songPart[j]} in ${songs[i]} by ${artists[i]}`);
+    }
     document.getElementById("printHere").innerHTML = data;
 }
 
